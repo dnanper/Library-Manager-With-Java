3e2f958019fac049701fc0faa2899bdec58c6174
cuong912005~ <nguyenthaccuong91@gmail.com>
2024-10-10 16:17:38 +0700

    setting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,14 +7,18 @@ module controller {
     requires com.jfoenix;
     requires java.sql;
     requires java.desktop;
-    //requires derbyclient;
+    //  requires derbyclient;
     requires derby;
+    requires com.google.gson;
 
+    opens ui.settings to javafx.fxml;
     opens ui.listmember to javafx.fxml;
     opens ui.addbook to javafx.fxml;
     opens ui.addmember to javafx.fxml;
     opens ui.listbook to javafx.fxml;
     opens ui.main to javafx.fxml;
+
+    exports ui.settings;
     exports ui.main;
     exports ui.listmember;
     exports ui.addbook;
