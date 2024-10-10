e56f9c01bae4b9fa363954c46d3aff1d736b65e8
cuong912005~ <nguyenthaccuong91@gmail.com>
2024-10-10 18:11:49 +0700

    setting

--- src/main/java/ui/settings/SettingsController.java
+++ src/main/java/ui/settings/SettingsController.java
@@ -1,15 +1,38 @@
 package ui.settings;
 
-import javafx.fxml.Initializable;
-
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXSpinner;
+import com.jfoenix.controls.JFXTextField;
+import java.io.File;
 import java.net.URL;
+import java.security.InvalidParameterException;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
 public class SettingsController implements Initializable {
 
+    @FXML
+    private JFXTextField nDaysWithoutFine;
+    @FXML
+    private JFXTextField finePerDay;
+    @FXML
+    private JFXTextField username;
+    @FXML
+    private JFXPasswordField password;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL url, ResourceBundle rb) {
 
     }
+    @FXML
+    private void handleSaveButtonAction(ActionEvent event) {
 
+    }
+    @FXML
+    private void handleCancelButtonAction(ActionEvent event) {
+
+    }
 }
